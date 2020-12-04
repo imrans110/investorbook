4d381c097a6f45d43bfb7c8c0f54ef4d2fdf14d1
Syed Imran Abidi <imrans110@hotmail.com>
2020-12-05 00:29:24 +0530

    Add initial configuration, Agenda font & Semantic UI Kit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import Investors from './Investors'
+import React from "react";
+import "./App.css";
+import Investors from "./components/Investors";
 
 function App() {
   return (
